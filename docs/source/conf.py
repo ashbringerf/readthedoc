977858dae03df1f4b97f9a87643e9b97c9fe50d5
ashbringerft <ashbringerft@gmail.com>
2022-05-09 23:38:48 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BullDong's notebook'
+copyright = '2022, FengTian'
+author = 'FengTian'
 
 release = '0.1'
 version = '0.1.0'
